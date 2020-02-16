9fc9d3926dd779ed3e58417764f6b267e6628779
ramit009 <ramit.jap@gmail.com>
2020-02-16 13:29:13 +0530

    Update HelloWorld.java
    updated msg

--- src/main/java/programmingKnowldege/HelloWorld.java
+++ src/main/java/programmingKnowldege/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		int s=0;
 		for(int i=0;i<args.length;i++) {
 			s= s + Integer.parseInt(args[i]);
-			System.out.println("sum is " + s);		
+			System.out.println("sum defined is " + s);		
 		}
 	
 		System.out.println("Hello World");
